aece2153d30d71a32ac6fb84ad98a0ef7319741c
Jhosua Saut Maruli <49725122+LuciferXtools@users.noreply.github.com>
2019-05-27 11:53:59 +0700

    Add files via upload

---
+++ Rika.sh
@@ -0,0 +1,94 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo $i"Hai Rika Monika"
+sleep 1
+figlet -f slant "WELCOME"
+slepp 1
+echo $i"hmmmm......"
+sleep 1 
+echo $i"Maaf kalau ngomong nya dari sini"
+sleep 1
+echo $cy"Soal nya kalau lewat WA nggk berani"
+sleep 1
+echo $cy"Sebenarnya aku......"
+sleep 1
+echo $i"Aku mau jujur ke kamu tentang.... "
+sleep 1
+echo $i"Tentang...."
+sleep 1
+echo $i"Tentang Persaan ku ke kamu"
+sleep 1
+echo $me"Yah...Meskipun kita beda agama :v"
+sleep 1
+echo $cy"Sebanar nya aku suka sama mu :v"
+sleep 1
+echo $cy"Tapi kamu ajh yg gk pekak"
+sleep 1
+echo $i"Kalau boleh.... 
+sleep 1
+figlet I LOVE YOU | lolcat
+echo $i"Kamu mau nggk Jadi pacar ku??? 
+echo $i"[A]. Ya"
+sleep 2
+echo $me"[B]. Tidak"
+sleep 2
+echo $pu"[C]. Tidak Mau Jawab"
+sleep 2
+echo $ku"[D]. Kagak Mau Bodoh Amat"
+sleep 2
+echo $cy"[E]. Keluar"
+echo
+read -p"JAWABANMU : " pil;
+
+if [ $pil = A ]
+then
+clear
+figlet -f slant "PRANK"
+sleep 3
+echo $i"Maaf ya...  cuma testing"
+echo $i"Jangan Marah Atuh... "
+fi
+
+if [ $pil = B ]
+then
+clear
+figlet -f slant "PRANK"
+sleep 3
+echo $i"Maaf ya...  cuma testing"
+echo $i"Jangan Marah Atuh... "
+fi
+
+if  [ $pil = C ]
+then
+clear
+figlet -f slant "PRANK"
+sleep 3
+echo $i"Maaf ya...  cuma testing"
+echo $i"Jangan Marah Atuh... "
+fi
+
+if [ $pil = D ]
+then
+clear
+figlet -f slant "PRANK"
+sleep 3
+echo $i"Maaf ya...  cuma testing"
+echo $i"Jangan Marah Atuh... "
+fi
+
+if [ $pil = E ]
+then
+clear
+figlet -f slant "PRANK"
+sleep 3
+echo $i"Maaf ya...  cuma testing"
+echo $i"Jangan Marah Atuh... "
+fi
